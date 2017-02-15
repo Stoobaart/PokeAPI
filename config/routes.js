95617a364fec484254b03af6cd4b29882a7def4e
Stuart Smith <stuartgordonsmith5@gmail.com>
2017-02-15 17:24:10 +0000

    config directory created

---
+++ config/routes.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var router = express.Router();
+var carsController = require('../controllers/pokemons');
+
+router.route('/')
+  .get(pokemonsController.index)
+  .post(pokemonsController.create);
+
+router.get('/new', pokemonsController.new);
+
+router.route('/:id')
+  .get(pokemonsController.show)
+  .put(pokemonsController.update)
+  .delete(pokemonsController.delete);
+
+router.get('/:id/edit', pokemonsController.edit);
+
+
+module.exports = router;
