843d13c65c2c0c1fc55a588e03d03af4872cdee2
Stuart Smith <stuartgordonsmith5@gmail.com>
2017-02-16 14:46:02 +0000

    required mongoose, body parser, layout and method override

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
+var router = require('./config/routes');
+var mongoose = require('mongoose');
+var layouts = require('express-ejs-layouts');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
+mongoose.connect('mongodb://localhost/pokemonDb');
 
-app.get('/', function(req, res) {
-    res.send('Request working.')
-  })
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride(function(req, res){
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    var method = req.body._method
+    delete req.body._method
+    return method
+  }
+}));
 
+app.set('view engine', 'ejs');
+
+app.use(layouts);
+
+app.use(router);
 
 app.listen(port , function(){
-  console.log('listening on port 3000');
+  console.log('listening on port ' + port);
 });
+
+module.exports = app;
