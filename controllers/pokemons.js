739eded1744de35e48a823c02d6479c723d88bd6
Stuart Smith <stuartgordonsmith5@gmail.com>
2017-02-16 14:48:35 +0000

    all functions for restful routes created and manually tested

--- controllers/pokemons.js
+++ controllers/pokemons.js
@@ -1,29 +1,84 @@
+var Pokemon = require('../models/pokemon');
+
 function indexPokemons(req, res) {
-	res.send('index');
+	Pokemon.find({}, function(err, pokemons) {
+		if(err) return res.status(500).send(err);
+		res.render("pokemons/index" , {
+			title: "Pokemon",
+			pokemons: pokemons
+		});
+	});
 }
 
 function showPokemons(req, res) {
-	res.send('show');
+	Pokemon.findById(req.params.id, function(err, pokemon) {
+		if(!pokemon) return res.status(404).send("Not Found");
+		if(err) return res.status(500).send(err);
+		res.render("pokemons/show", {
+			title: "Pokemon",
+			pokemon: pokemon
+		});
+	});
 }
 
 function newPokemons(req, res) {
-	res.send('new');
+	var newPokemon = {
+		id: "",
+		url: "",
+		name: "",
+	    national_id: 0,
+	    abilities: "",
+	    evolutions: "",
+	    descriptions: "",
+	    moves: "",
+	    hp: 0,
+	    attack: 0,
+	    defence: 0,
+	    speed: 0
+	}
+
+	res.render("pokemons/new", {
+	title: "New Pokemon",
+	pokemon: newPokemon
+	});
 }
 
 function createPokemons(req, res) {
-	res.send('create');
+	Pokemon.create(req.body, function(err, pokemon) {
+		if(err) return res.status(500).send(err);
+		res.redirect("/");
+	});
 }
 
 function editPokemons(req, res) {
-	res.send('edit');
+	Pokemon.findById(req.params.id, function(err, pokemon){
+		if(!pokemon) return res.status(404).send("Not Found");
+		if(err) return res.status(500).send(err);
+		console.log(pokemon)
+		res.render("pokemons/edit", {
+			title: "Pokemon",
+			pokemon: pokemon
+		});
+	});
 }
 
 function updatePokemons(req, res) {
-	res.send('update');
+	console.log(req.params.id);
+	Pokemon.findByIdAndUpdate(
+		req.params.id, 
+		{ $set: req.body},
+		{ runValidators: true, new: true },
+		function(err, pokemon){
+			if(err) return res.status(500).send(err);
+			res.redirect("/");
+		}
+	);
 }
 
 function deletePokemons(req, res) {
-	res.send('delete');
+	Pokemon.findByIdAndRemove(req.params.id, function(err) {
+		res.redirect("/")
+	});
 }
 
 module.exports = {
