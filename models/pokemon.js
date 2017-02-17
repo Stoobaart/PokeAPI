2e3405ebdd3547405e9126d531af4cfd193f04d7
Stuart Smith <stuartgordonsmith5@gmail.com>
2017-02-17 10:07:24 +0000

    ollie turning the dot green

---
+++ models/pokemon.js
@@ -0,0 +1,48 @@
+var mongoose = require('mongoose');
+
+var PokemonSchema = mongoose.Schema({
+  url: {
+    type: String
+  },
+  name: { 
+  	type: String, 
+  	required: true
+  },
+  national_id: {
+  	type: Number,
+  },
+  abilities: { 
+  	type: String, 
+  	required: true
+  },
+  evolutions: { 
+  	type: String, 
+  	required: true
+  },
+  descriptions: { 
+  	type: String, 
+  	required: true
+  },
+  moves: { 
+  	type: String, 
+  	required: true
+  },
+  hp: {
+  	type: Number, 
+  	required: true
+  },
+  attack: {
+  	type: Number, 
+  	required: true
+  },
+  defence: {
+  	type: Number, 
+  	required: true
+  },
+  speed: {
+  	type: Number, 
+  	required: true
+  }
+})
+
+module.exports = mongoose.model('Pokemon', PokemonSchema);
