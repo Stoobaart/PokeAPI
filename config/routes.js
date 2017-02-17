3d3848b2637f7ac9b04a2baab1b73c7525ae4cd2
Stuart Smith <stuartgordonsmith5@gmail.com>
2017-02-17 09:59:36 +0000

    created routes for sessions and users

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,23 @@
 var express = require("express");
 var router = express.Router();
 var pokemonsController = require('../controllers/pokemons');
+var usersController = require('../controllers/users');
+var sessionsController = require('../controllers/sessions');
+
+// sessions
+router.route('/sessions')
+      .post(sessionsController.create)
+      .delete(sessionsController.delete);
+
+router.route('/sessions/new')
+      .get(sessionsController.new);
+
+// users
+router.route('/users')
+      .post(usersController.create);
+
+router.route('/users/new')
+      .get(usersController.new);
 
 router.route('/')
   .get(pokemonsController.index)
@@ -14,6 +31,5 @@ router.route('/:id')
   .delete(pokemonsController.delete);
 
 router.get('/:id/edit', pokemonsController.edit);
-
 
 module.exports = router;
