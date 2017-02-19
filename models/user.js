1de2923d16bd0a7e1b9ba22dff55b1abac0bba08
Stuart Smith <stuartgordonsmith5@gmail.com>
2017-02-19 13:45:33 +0000

    created new user model

--- models/user.js
+++ models/user.js
@@ -4,7 +4,8 @@ var UserSchema = new mongoose.Schema({
   first_name : {type: String, required: true},
   last_name : {type: String, required: true},
   email : {type: String, required: true},
-  password : {type: String, required: true}
+  password : {type: String, required: true},
+  pokemons : [{ type: mongoose.Schema.Types.ObjectId, ref: "Pokemon" }]
 });
 
 module.exports = mongoose.model('User' , UserSchema);
